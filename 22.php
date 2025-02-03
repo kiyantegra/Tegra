4d742d5e95f66910941ce83dca489ce1af0ef00f
kiyantegra <141841630+kiyantegra@users.noreply.github.com>
2025-02-03 10:01:22 +0330

    Update 22.php

--- 22.php
+++ 22.php
@@ -1,21 +1,8 @@
 <?php
-//******** لایسنس خود را وارد کنید  ********/
-license = "6TTZ5-TZ3NF-JYW2U-ADS7U";
 
-//******** این بخش را دست نزنید ********/
-$c = curl_init();
-$verifyUrl = "https://raw.githubusercontent.com/kiyantegra/Tegra/refs/heads/main/22.php={license}";
-curl_setopt($c, CURLOPT_URL, $verifyUrl);
-curl_setopt($c, CURLOPT_TIMEOUT, 30);
-curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-
-$curlresult = curl_exec($c);
-$decodedResult = json_decode($curlresult, true);
-
-if ($decodedResult && isset($decodedResult['status']) && $decodedResult['status'] === 'success') {
-//******** این بخش را دست نزنید ********/
-
-} else {
-// اگر لایسنس معتبر نباشد
-echo "لایسنس نامعتبر است!";
+if (empty($_REQUEST["serial"]) || ($_REQUEST["serial"] != "2017"))
+{
+    header("HTTP/1.0 401 The serial number is not valid");
+    // error message to be displayed in installer
+    echo "The serial number is not valid";
 }
