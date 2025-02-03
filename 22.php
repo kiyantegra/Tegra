21278df41758e61704f5be618c2d0ff7b9a461c2
kiyantegra <141841630+kiyantegra@users.noreply.github.com>
2025-02-03 07:46:57 +0330

    Update 22.php

--- 22.php
+++ 22.php
@@ -1,10 +1,10 @@
 <?php
 //******** لایسنس خود را وارد کنید  ********/
-$license = "6TTZ5-TZ3NF-JYW2U-ADS7U";
+license = "6TTZ5-TZ3NF-JYW2U-ADS7U";
 
 //******** این بخش را دست نزنید ********/
 $c = curl_init();
-$verifyUrl = "https://app.zelic.ir/verifyLicenseJson?lic={$license}";
+$verifyUrl = "https://app.zelic.ir/verifyLicenseJson?lic={license}";
 curl_setopt($c, CURLOPT_URL, $verifyUrl);
 curl_setopt($c, CURLOPT_TIMEOUT, 30);
 curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
