73922c9b8568455dcd102ff0bfa1cc051a277dcf
kiyantegra <141841630+kiyantegra@users.noreply.github.com>
2025-02-03 07:46:03 +0330

    Add files via upload

---
+++ 22.php
@@ -0,0 +1,21 @@
+<?php
+//******** لایسنس خود را وارد کنید  ********/
+$license = "6TTZ5-TZ3NF-JYW2U-ADS7U";
+
+//******** این بخش را دست نزنید ********/
+$c = curl_init();
+$verifyUrl = "https://app.zelic.ir/verifyLicenseJson?lic={$license}";
+curl_setopt($c, CURLOPT_URL, $verifyUrl);
+curl_setopt($c, CURLOPT_TIMEOUT, 30);
+curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+
+$curlresult = curl_exec($c);
+$decodedResult = json_decode($curlresult, true);
+
+if ($decodedResult && isset($decodedResult['status']) && $decodedResult['status'] === 'success') {
+//******** این بخش را دست نزنید ********/
+
+} else {
+// اگر لایسنس معتبر نباشد
+echo "لایسنس نامعتبر است!";
+}
